95cda3b8f5d3695f5c65bedf4e1c9a53fadc56ef
Piotr Sitarz <piotrsitarz@onet.eu>
2019-05-07 22:22:49 +0200

    prettier

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import Converter from './components/Converter/Converter';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render( < Converter / > , document.getElementById('root'));
+ReactDOM.render(<Converter />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
